92de7c24585e047f7deca7be27f17b7ff9ba069a
Sergey Dziadevich <sergey_dziadevich@epam.com>
2019-07-08 11:26:48 +0300

    add admin.modul, admin-routing.module  components:  ManageOrders, ManageProducts, AdminDashboard

---
+++ src/app/admin/admin-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AdminComponent } from './admin.component';
+import { AdminDashboardComponent, ManageOrdersComponent, ManageProductsComponent } from './components';
+
+const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        children: [
+          { path: 'products', component: ManageProductsComponent },
+          { path: 'orders', component: ManageOrdersComponent },
+          { path: '', component: AdminDashboardComponent }
+        ]
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdminRoutingModule { }
+//export const adminRouterComponents = [AdminComponent, AdminDashboardComponent, ManageProductsComponent, ManageOrdersComponent];
