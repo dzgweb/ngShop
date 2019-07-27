ddbe1905eb296914b649848d00b02d753b29d147
Serge Dziadevich <developersiteweb@gmail.com>
2019-07-27 16:38:47 +0300

    update angular to 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,12 @@ import { CartModule } from './cart/cart.module';
 import { AdminModule } from './admin/admin.module';
 import { AppComponent } from './app.component';
 import { OrdersModuleModule } from './orders-module/orders-module.module';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
@@ -23,10 +25,10 @@ import { OrdersModuleModule } from './orders-module/orders-module.module';
     SharedModule,
     CartModule,
     AdminModule,
+    OrdersModuleModule,
     CoreModule,
     // AppRoutingModule MUST BE LAST
-    AppRoutingModule,
-    OrdersModuleModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
