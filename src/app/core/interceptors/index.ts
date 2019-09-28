56bf16fd3d92fefea0e468fbaa0986df6a540d81
ÐSergey Dziadevich <developersiteweb@gmail.com>
2019-09-28 09:44:58 +0300

    Timing Interceptor

--- src/app/core/interceptors/index.ts
+++ src/app/core/interceptors/index.ts
@@ -1,11 +1,17 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { TsInterceptor } from './ts.interceptor';
+import { TimingInterceptor } from './timing.interceptor';
 
 export const httpInterceptorProviders = [
   {
     provide: HTTP_INTERCEPTORS,
     useClass: TsInterceptor,
+    multi: true
+  },
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TimingInterceptor,
     multi: true
   }
 ];
